10e45c1ac029938f3011a9946df36234b5938acb
David Schneider <d.a.schneider195@gmail.com>
2020-10-12 12:19:35 +0200

    Implemented collection for rating, rating_count, name, time, difficulty

--- crawler/CrawledArticle.py
+++ crawler/CrawledArticle.py
@@ -1,5 +1,16 @@
 class CrawledArticle():
-    def __init__(self, title="Init_title", link="", num_rep=0):
-        self.link = link
+    def __init__(self, title="Init_title",
+                 rating=0.0, rating_c=0,
+                 num_rep=0, time=0,
+                 difficulty="normal", person_c=1,
+                 ingredients=[], description="", tags=[]):
+        self.rating_count = rating_c
+        self.time = time
+        self.difficulty = difficulty
+        self.person_count = person_c
+        self.ingredients = ingredients
+        self.description = description
+        self.tags = tags
+        self.rating = rating
         self.title = title
         self.num_rep = num_rep
