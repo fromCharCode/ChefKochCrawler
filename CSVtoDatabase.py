891baa4aaba221fbe6d34d915bc8e458365e2d63
Tobi <ytacc87@gmail.com>
2020-10-14 23:04:01 +0200

    files added

--- CSVtoDatabase.py
+++ CSVtoDatabase.py
@@ -1,37 +1,68 @@
 import sqlite3
-
+import time
+import ast
 import pandas as pd
 
 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
+
+def insert_recipe(title, rating, rating_c, recipe_time, difficulty, person_c, ingredients, description, tags):
     try:
-        conn = sqlite3.connect(db_file)
-    except sqlite3.Error as e:
-        print(e)
-
-    return conn
-
-
-def insert_recipe():
-    try:
-        test = pd.DataFrame(pd.read_csv("all_articles.csv"))
 
         sqliteConnection = sqlite3.connect('recipe_database')
         cursor = sqliteConnection.cursor()
         print("Successfully Connected to SQLite")
 
-        sqlite_insert_query = ''' INSERT INTO recipe_database()
-                 VALUES() '''
+        sqlite_insert_with_param = """ INSERT INTO all_recipes(title, rating, rating_c, recipe_time, difficulty, person_c, ingredients, description, tags)
+                             VALUES(?,?,?,?,?,?,?,?,?); """
 
-        count = cursor.execute(sqlite_insert_query)
+        data_tuple = (title, rating, rating_c, recipe_time, difficulty, person_c, ingredients, description, tags)
+        cursor.executemany(sqlite_insert_with_param, (data_tuple,))
         sqliteConnection.commit()
         print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
         cursor.close()
+    except sqlite3.Error as error:
+        print("Failed to insert data into sqlite table", error)
 
-insert_recipe()
+    finally:
+        if (sqliteConnection):
+            sqliteConnection.close()
+            print("The SQLite connection is closed")
+
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo=False)
+test = pd.read_csv("all_articles.csv", chunksize=1)
+test2 = pd.DataFrame(test)
+test2.to_sql(name="test", con=engine)
+
+print(test2)
+
+i = 0
+
+for item in test:
+    time.sleep(3)
+    i += 1
+    """
+    title = item["title"]
+    rating = item["rating"]
+    rating_c = item["rating_c"]
+    recipe_time = item["recipe_time"]
+    difficulty = item["difficulty"]
+    person_c = item["person_c"]
+    ingredients = item["ingredients"]
+    description = item["description"]
+    tags = item["tags"]
+"""
+
+
+
+    # print(title)
+    # print(rating)
+    # print(tags)
+
+    #insert_recipe(title, rating, rating_c, recipe_time, difficulty, person_c, ingredients, description, tags)
+
+#insert_recipe('test', 4.5, 4, 10, 'test', 4, 'test', 'test', 'test')
+
+''''            item["title"], item["rating"], item["rating_c"], item["recipe_time"], item[
+                "difficulty"], item["person_c"], item["ingredients"], item["description"], item["tags"]'''
